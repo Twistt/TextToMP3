764c8e5650f438618ee035117637d5581b9542f0
Twist <twistedsprites@gmail.com>
2018-11-06 14:36:49 -0700

    Initial Commit - Fully functional as is

---
+++ TwistedTTS.Desktop/TextToMp3.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TwistedTTS.Desktop
+{
+    partial class TextToMp3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextToMp3));
+            this.rtfText = new System.Windows.Forms.RichTextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackbarPosition = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ddlVoices = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackbarPosition)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtfText
+            // 
+            this.rtfText.Location = new System.Drawing.Point(12, 12);
+            this.rtfText.Name = "rtfText";
+            this.rtfText.Size = new System.Drawing.Size(572, 249);
+            this.rtfText.TabIndex = 0;
+            this.rtfText.Text = resources.GetString("rtfText.Text");
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(139, 288);
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(196, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_ValueChanged);
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // trackbarPosition
+            // 
+            this.trackbarPosition.Location = new System.Drawing.Point(12, 413);
+            this.trackbarPosition.Maximum = 100;
+            this.trackbarPosition.Name = "trackbarPosition";
+            this.trackbarPosition.Size = new System.Drawing.Size(374, 45);
+            this.trackbarPosition.TabIndex = 5;
+            this.trackbarPosition.TickFrequency = 5;
+            this.trackbarPosition.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackbarPosition.Scroll += new System.EventHandler(this.trackbarPosition_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Speed of Speech";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 398);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Volume";
+            // 
+            // ddlVoices
+            // 
+            this.ddlVoices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlVoices.FormattingEnabled = true;
+            this.ddlVoices.Location = new System.Drawing.Point(12, 292);
+            this.ddlVoices.Name = "ddlVoices";
+            this.ddlVoices.Size = new System.Drawing.Size(121, 21);
+            this.ddlVoices.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(341, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(242, 26);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Save TTS to MP3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(392, 414);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(192, 45);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.TickFrequency = 5;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(139, 384);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Play/Pause";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Voice";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // TextToMp3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 463);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.ddlVoices);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackbarPosition);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.rtfText);
+            this.Name = "TextToMp3";
+            this.Text = "TTS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackbarPosition)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtfText;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackbarPosition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ddlVoices;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TwistedTTS.Desktop/TextToMp3.cs
@@ -0,0 +1,214 @@
+﻿using NAudio.Lame;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwistedTTS.Desktop
+{
+    public partial class TextToMp3 : Form
+    {
+        private int _speechRate = 0;
+        private int _speechVolume = 50;
+        // Initialize a new instance of the SpeechSynthesizer.  
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        // Configure the audio output.   
+        private string LastSavedFile = "";
+        public TextToMp3()
+        {
+            timer.Interval = 10;
+            timer.Tick += (o, e) =>
+            {
+                if (audioFile != null)
+                {
+                    trackbarPosition.Value = (int)audioFile.Position;
+                }
+            };
+            InitializeComponent();
+            AddInstalledVoicesToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            synth.SetOutputToDefaultAudioDevice();
+            // Subscribe to the StateChanged event.  
+            synth.StateChanged += new EventHandler<StateChangedEventArgs>(synth_StateChanged);
+            // Subscribe to the SpeakProgress event.  
+            synth.SpeakProgress += new EventHandler<SpeakProgressEventArgs>(synth_SpeakProgress);
+            // Subscribe to the SpeakCompleted event.  
+            synth.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(synth_SpeakCompleted);
+
+
+            trackBar1.Scroll += (s, a) => outputDevice.Volume = trackBar1.Value / 100f;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            synth = new SpeechSynthesizer { Volume = _speechVolume, Rate = _speechRate };
+            synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Child);
+            synth.SpeakAsync(rtfText.Text);
+            trackbarPosition.Minimum = 0;
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (synth.State == SynthesizerState.Speaking) synth.Pause();
+            else synth.Resume();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Cancel the SpeakAsync operation and wait one second.  
+            synth.SpeakAsyncCancelAll();
+            Thread.Sleep(1000);
+            Console.WriteLine("Redirecting output");
+            synth.SetOutputToWaveFile(@"d:\test.wav");
+            Console.WriteLine("Speaking text...");
+            synth.Speak("This is a test.");
+            Console.WriteLine("Done speaking text");
+            synth.SetOutputToNull();
+            Console.WriteLine("Done setting output to null");
+        }
+
+        // Write to the console when the SpeakAsync operation has been cancelled.  
+        static void synth_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            Console.WriteLine("The SpeakAsync operation was cancelled!!");  
+        }
+
+        // When it changes, write the state of the SpeechSynthesizer to the console.  
+        static void synth_StateChanged(object sender, StateChangedEventArgs e)
+        {
+            Console.WriteLine("Synthesizer State: {0} Previous State: {1}", e.State, e.PreviousState);  
+        }
+
+        // Write the text being spoken by the SpeechSynthesizer to the console.  
+        static void synth_SpeakProgress(object sender, SpeakProgressEventArgs e)
+        {
+            Console.WriteLine(e.Text);
+            Console.WriteLine(e.AudioPosition);
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            _speechRate = trackBar2.Value;
+            synth.Rate = _speechRate;
+        }
+        private void AddInstalledVoicesToList()
+        {
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                foreach (var voice in synth.GetInstalledVoices())
+                {
+                    ddlVoices.Items.Add(voice.VoiceInfo.Name);
+                }
+            }
+
+            ddlVoices.SelectedIndex = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            synth.SelectVoice(ddlVoices.SelectedItem.ToString());
+            if(audioFile != null) audioFile.Close();
+            synth.SpeakAsyncCancelAll();
+            Thread.Sleep(1000);
+            Console.WriteLine("Redirecting output");
+            synth.SetOutputToWaveFile("temp.wav");
+            Console.WriteLine("Speaking text...");
+            synth.Speak(rtfText.Text);
+            Console.WriteLine("Done speaking text");
+            synth.SetOutputToNull();
+            Console.WriteLine("Done setting output to null");
+            SaveFileDialog ofd = new SaveFileDialog();
+            ofd.Filter = "Text files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            ofd.ShowDialog();
+            using (var reader = new AudioFileReader("temp.wav"))
+            using (var writer = new LameMP3FileWriter(ofd.FileName, reader.WaveFormat, LAMEPreset.ABR_128))
+                reader.CopyTo(writer);
+            LastSavedFile = ofd.FileName;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (outputDevice != null && outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                outputDevice.Pause();
+                timer.Stop();
+            }
+            else if (outputDevice != null && outputDevice.PlaybackState == PlaybackState.Paused)
+            {
+                outputDevice.Play();
+                timer.Start();
+            }
+            else
+            {
+                outputDevice = new WaveOutEvent();
+                audioFile = new AudioFileReader(LastSavedFile);
+                trackbarPosition.Maximum = (int)audioFile.Length;
+                trackbarPosition.TickFrequency = (int)(audioFile.Length / 100);
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+
+                outputDevice.Init(audioFile);
+                timer.Start();
+                outputDevice.Play();
+            }
+
+        }
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            timer.Stop();
+            //outputDevice.Dispose();
+            //outputDevice = null;
+            //audioFile.Dispose();
+            //audioFile = null;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (outputDevice != null)
+            {
+                outputDevice.Dispose();
+                outputDevice = null;
+            }
+            if (audioFile != null)
+            {
+                audioFile.Dispose();
+                audioFile = null;
+            }
+        }
+
+        private void trackbarPosition_Scroll(object sender, EventArgs e)
+        {
+            audioFile.Position = trackbarPosition.Value;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (outputDevice != null && audioFile != null)
+            {
+                timer.Stop();
+                outputDevice.Stop();
+                audioFile.Position = 0;
+            }
+        }
+    }
+}
